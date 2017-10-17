3db342d7194b234e9f9f12ea71387145464cd39e
Tim <garif-tim@yandex.ru>
2017-10-17 12:16:06 +0300

    dz 3+7

---
+++ dz3_4.cpp
@@ -0,0 +1,74 @@
+﻿/*Дан отсортированный массив различных целых чисел A[0..n-1] и массив целых чисел B[0..m-1]. 
+Для каждого элемента массива B[i] найдите минимальный индекс элемента массива A[k], ближайшего по значению к B[i]. Время работы поиска для каждого элемента B[i]: O(log(k)).
+n ≤ 110000, m ≤ 1000.
+*/
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int minNearNum(int* mas, int count, int num) {
+	int last = count - 1;
+	int first = 0;
+
+	if (num <= mas[first])
+		return 0;
+	if (num >= mas[last])
+		return last;
+
+//  Уменьшаем границу для последующего поиска
+	for (int ind = 1; ind < last; ind <<= 1) {
+		if (mas[ind] < num)
+			first = ind;
+		else
+			last = ind;
+	}
+
+//  Поиск
+	while (first < last - 1) {
+		int middle = (last + first) / 2;
+		if (mas[middle] < num)
+			first = middle;
+		else
+			last = middle;
+	}
+
+//  Высчитываем разницу между самим числом и числов в массиве
+//  и выбираем наименьшую
+	int d_left = num - mas[first];
+	int d_right = mas[last] - num;
+
+	if (d_left > d_right)
+		return last;
+	else
+		return first;
+}
+
+int main() {
+	int n = 0, m = 0, k = 0;
+	int i = 0;
+
+	cin >> n;
+	assert(n > 0);
+	int* masA = new int[n];
+	for (i = 0; i < n; i++) {
+		cin >> masA[i];
+	}
+
+	cin >> m;
+	assert(m > 0);
+	int* masB = new int[m];
+	for (i = 0; i < m; i++) {
+		cin >> masB[i];
+	}
+
+	for (i = 0; i < m; i++) {
+		k = minNearNum(masA, n, masB[i]);
+		cout << k << " ";
+	}
+	cout << endl;
+
+	delete[] masA;
+	delete[] masB;
+	return 0;
+}
---
+++ dz7_1.cpp
@@ -0,0 +1,82 @@
+﻿/*В большой IT - фирме есть только одна переговорная комната.Желающие посовещаться заполняют заявки с желаемым временем начала и конца.Ваша задача определить максимальное количество заявок, которое может быть удовлетворено.
+Число заявок ≤ 100000.
+Формат входных данных :
+Вход содержит только пары целых чисел — начала и концы заявок.
+Формат выходных данных :
+Выход должен содержать натуральное число — максимальное число заявок.
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Meeting {
+private:
+	int start;
+	int end;
+public:
+	Meeting(int st_, int end_) : start(st_), end(end_) {}
+	int getStart() { return start; }
+	int getEnd() { return end; }
+};
+
+int compare(const void *x1, const void *x2) {
+	Meeting **m1 = (Meeting**)x1;
+	Meeting **m2 = (Meeting**)x2;
+	return ((*m1)->getEnd() - (*m2)->getEnd());
+}
+
+//Решаем жадным алгоритмом. Сортируем по времени окончания и берем,
+//если начало следующего не входит в отрезок предыдущего
+size_t maxRequests(Meeting **mas, size_t max) {
+	qsort(mas, max, sizeof(Meeting*), compare);
+	size_t count = 1;
+
+
+	for (size_t i = 1, j = 0; i < max; i++) {
+		if (mas[i]->getStart() >= mas[j]->getEnd()) {
+			j = i; 
+			count++;
+		}
+	}
+	return count;
+}
+
+size_t inputMeets(Meeting ***top) {
+	Meeting **mas = NULL;
+	Meeting **masBuf = NULL;
+	int st = 0,
+		en = 0;
+	size_t newMasLen = 0;
+	size_t meetNum = 0;
+
+	while (cin >> st >> en) {
+		if (newMasLen <= meetNum) {
+			newMasLen = newMasLen ? newMasLen * 2 : 1;
+			masBuf = new Meeting* [newMasLen];
+			for (size_t i = 0; i < meetNum; i++) {
+				masBuf[i] = mas[i];
+			}
+			delete[] mas;
+			mas = masBuf;
+		}
+		Meeting *newMeet = new Meeting(st, en);
+		mas[meetNum] = newMeet;
+		meetNum++;
+	}
+	*top = mas;
+	return meetNum;
+}
+
+int main() {
+	Meeting **meets = NULL;
+	size_t meetNum = inputMeets(&meets);
+	size_t maxMeet = maxRequests(meets, meetNum);
+
+	cout << maxMeet << endl;
+
+	for (size_t i = 0; i < meetNum; i++)
+		delete meets[i];
+	delete[] meets;
+	return 0;
+}
